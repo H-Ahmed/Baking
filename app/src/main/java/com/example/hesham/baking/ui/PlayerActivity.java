3fa9de91497f425db88b349716615fefb5f3ce86
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-09-25 15:20:47 +0200

    start in fragment

---
+++ app/src/main/java/com/example/hesham/baking/ui/PlayerActivity.java
@@ -0,0 +1,73 @@
+package com.example.hesham.baking.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.example.hesham.baking.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+
+import static com.example.hesham.baking.ui.composer.StepFragment.VIDEO_URL;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+
+    private String videoURL;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        playerView = findViewById(R.id.player_view);
+    }
+
+    private void closeActivity() {
+        finish();
+        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(VIDEO_URL)) {
+            videoURL = intent.getStringExtra(VIDEO_URL);
+        } else {
+            closeActivity();
+        }
+
+        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
+        playerView.setPlayer(player);
+
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, String.valueOf(R.string.app_name)));
+
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(videoURL));
+
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        playerView.setPlayer(null);
+        player.release();
+        player = null;
+    }
+
+}
